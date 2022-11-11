4872cd8a8aa158636512136c68ca9bb83f61fa19
Abhishekjoshi98 <98544171+Abhishekjoshi98@users.noreply.github.com>
2022-11-11 11:14:23 +0530

    Update Movie.js

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 
 const Movie = () => {
   const [movieData, setMovieData] = useState([]);
-  const [MovieTitle, setMovieTitle] = useState('Marvel');
+  const [MovieTitle, setMovieTitle] = useState();
   const [isClicked, setIsClicked] = useState(false);
 
   useEffect(() => {
